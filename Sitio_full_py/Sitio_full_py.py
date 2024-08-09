b6b887a977ce0c13398afac59ae5818997b41d4e
Guillermo <memorosas.00@gmail.com>
2024-08-09 16:26:26 -0600

    se agrega barra de navegacion

--- Sitio_full_py/Sitio_full_py.py
+++ Sitio_full_py/Sitio_full_py.py
@@ -1,6 +1,7 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 from tkinter.tix import Tree
+from turtle import position
 import reflex as rx
 from sqlalchemy import true
 
@@ -18,8 +19,99 @@ class State(rx.State):
         # esta impresion se haria en consola y no se muestra en el front
         print ("hello worl")
 
+def navbar_link(text: str, url: str) -> rx.Component:
+    return rx.link(
+        rx.text(text, size="4", weight="medium"), href=url
+    )
+
+#  Sorce https://reflex.dev/docs/recipes/layout/navbar/
+
 def navigator_bar() -> rx.Component:
-    return rx.heading("Saas App", size="9")
+    return rx.box(
+        #position=="top-left",
+        rx.desktop_only(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2.25em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="7", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.hstack(
+                    navbar_link("Home", "/#"),
+                    navbar_link("About", "/#"),
+                    navbar_link("Pricing", "/#"),
+                    navbar_link("Contact", "/#"),
+                    spacing="5",
+                ),
+                rx.hstack(
+                    rx.button(
+                        "Sign Up",
+                        size="3",
+                        variant="outline",
+                    ),
+                    rx.button("Log In", size="3"),
+                    spacing="4",
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        # estos son para cuando se ajusta exactamente al tamaño
+        rx.mobile_only(),
+        rx.tablet_only(),
+        
+        rx.mobile_and_tablet(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="6", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.menu.root(
+                    rx.menu.trigger(
+                        rx.icon("menu", size=30)
+                    ),
+                    rx.menu.content(
+                        rx.menu.item("Home"),
+                        rx.menu.item("About"),
+                        rx.menu.item("Pricing"),
+                        rx.menu.item("Contact"),
+                        rx.menu.separator(),
+                        rx.menu.item("Log in"),
+                        rx.menu.item("Sign up"),
+                    ),
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+
+        # Aqui estan las propiedades de la barra de navegacion
+        bg=rx.color("accent", 3),
+        padding="1em",
+        # position="fixed",
+        # top="0px",
+        # z_index="5",
+        width="100%",
+    )
+
+
 
 # cada argumento corresponde a un rx.**
 #def base_page(a, b, c) -> rx.Component:
@@ -45,6 +137,7 @@ def base_page(child: rx.Component, hide_navbar =False, *args, **kwargs) -> rx.Co
         #*args,
 
         # otra forma de en lugar de args es agregar child: rx.Component en los parametros y:
+        # invocamos el nuevo codigo
         navigator_bar(),
         child,
 
@@ -100,8 +193,7 @@ def index() -> rx.Component:
             spacing="5",
             justify="center",
             min_height="85vh",
-        ),
-        hide_navbar=True
+        )        
         
     )
 
