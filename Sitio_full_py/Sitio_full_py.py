092d9a6d063dfba61bae746f2e3eea1eb0492574
Guillermo <memorosas.00@gmail.com>
2024-08-13 10:35:11 -0600

    se agrega la pagina acerca de nosotros

--- Sitio_full_py/Sitio_full_py.py
+++ Sitio_full_py/Sitio_full_py.py
@@ -78,44 +78,21 @@ def index() -> rx.Component:
     return base_page(my_child)
 
 
-""" Se genera nueva pagina acerca de """
+""" Se genera nueva pagina acerca de nosotros """
 
-def about_page() -> rx.Component:
-    # Welcome Page (Index) , return our base_page   
-    # return base_page("Hola")
+def about_page() -> rx.Component:    
 
     # Documentacion aqui https://reflex.dev/docs/library/layout/stack/#vstack
     my_child = rx.vstack(
             rx.heading("About us", size="9"),
             rx.text(
-                "Usted puede iniciar por editar: ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="4",
+                "Conozca mas sobre nosotros ",                
             ),
-
-            # On clik es un evento, s eme habia olvidado la coma despues de parentesis
-            # rx.button("hacer algo", on_click=State.cambiar_etiqueta),
-
-            # Esto es algo dinamico
-
-
-
-            rx.input(
-                # guardamos el texto escrito en el texbox, y no se borra aunque recargues
-                default_value=State.label,
-                on_click=State.did_clic(),
-                on_change= State.handle_title_input_change),
-
-
-            rx.link(
-                rx.button("Para mayor informacion puede ir a: "),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
+           
             spacing="5",
             justify="center", # tenia centar en lugar de center
             align="center", # podemos usar end, strech et etc
-            text_align="center",
+            
             min_height="85vh",
             id="my_child"
         )
@@ -126,3 +103,6 @@ def about_page() -> rx.Component:
 
 app = rx.App()
 app.add_page(index)
+
+# Agregamos nuestra pagina nueva (*** /ruta)
+app.add_page(about_page, route='/aboutus')
