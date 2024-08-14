dbbe32ce31e095a779af9bafdce9d63071cac303
Guillermo <memorosas.00@gmail.com>
2024-08-14 14:46:04 -0600

    se crea el archivo navigator-state

---
+++ Sitio_full_py/navigation/state.py
@@ -0,0 +1,35 @@
+
+
+import reflex as rx
+
+from . import routes
+
+class NavState(rx.State):
+
+    def to_home(self):
+        
+        # vamos a la pagina home
+        return rx.redirect(routes.HOME_ROUTE)
+    
+    def to_about_us (self):
+
+        return rx.redirect(routes.ABOUT_US_ROUTE)
+    
+    def to_pricing (self):
+
+        return rx.redirect(routes.PRICING_ROUTE)
+    
+    def to_contact (self):
+
+        return rx.redirect(routes.CONTACT_ROUTE)
+    
+    def to_mis_dispositivos (self):
+
+        return rx.redirect(routes.MIS_DISPOSITIVOS_ROUTE)
+    
+    def to_suscripciones (self):
+
+        return rx.redirect(routes.SUSCRIPCIONES_ROUTE)
+    
+    
+        
