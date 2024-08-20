1794060bc199240ffea2b6d8115427d45a329b2a
Guillermo <memorosas.00@gmail.com>
2024-08-20 10:59:55 -0600

    se agrega la pagina devices pero aun no funciona

--- Sitio_full_py/pages/__init__.py
+++ Sitio_full_py/pages/__init__.py
@@ -3,12 +3,13 @@ from .about import about_page
 from .pricing import pricing_page
 
 from .contact import contact_page
-
+from .devices import devices_page
 
 # aqui iran los metodos
 
 __all__ = [
     'about_page',
     'pricing_page',
-    'contact_page'
+    'contact_page',
+    'devices_page',
 ]
---
+++ Sitio_full_py/pages/devices.py
@@ -0,0 +1,27 @@
+
+import reflex as rx
+
+from .. ui.base import base_page
+
+
+""" Se genera nueva pagina datos """
+
+def devices_page() -> rx.Component:    
+
+    # Documentacion aqui https://reflex.dev/docs/library/layout/stack/#vstack
+    my_child = rx.vstack(
+            rx.heading("Quote", size="9"),
+            rx.text(
+                "Data show ",                
+            ),
+           
+            spacing="5",
+            justify="center", # tenia centar en lugar de center
+            align="center", # podemos usar end, strech et etc
+            
+            min_height="85vh",
+            id="my_child"
+        )
+
+    # Base page gets my_child
+    return base_page(my_child)
