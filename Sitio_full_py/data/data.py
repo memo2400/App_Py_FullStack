bb5eef7c849dbfc9c1147cf0928d74715629a105
Guillermo <memorosas.00@gmail.com>
2024-08-21 17:20:27 -0600

    se crea pagina para tratar los datos

---
+++ Sitio_full_py/data/data.py
@@ -0,0 +1,21 @@
+
+""" crear diccionario https://www.simplilearn.com/dictionary-in-python-article"""
+
+
+
+
+
+"""llenar listaa con diccioarios
+    https://www.askpython.com/python/list/list-of-dictionaries
+"""
+
+ls_dict = []
+
+
+dict_1 = {'x': 50, 'y': 45, 'z': 65}
+dict_2 = {'x': 66, 'y': 22, 'z': 89}
+
+ls_dict.append(dict_1)
+ls_dict.append(dict_2)
+
+print(ls_dict[1])
