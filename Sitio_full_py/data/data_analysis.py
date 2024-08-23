f5c32f13ede5f66e2154f4c2a62b9702c22ceb2f
Laptop <memorosas.00@gmail.com>
2024-08-23 16:34:59 -0600

    se siguen ahciendo pruebas en datos

--- Sitio_full_py/data/data_analysis.py
+++ Sitio_full_py/data/data_analysis.py
@@ -12,14 +12,14 @@ test_dict ['width'] = 5.2
     https://www.askpython.com/python/list/list-of-dictionaries
 """
 
-ls_dict = []
+ls_dict_test = []
 
 
 dict_1 = {'x': 50, 'y': 45, 'z': 65}
 dict_2 = {'x': 66, 'y': 22, 'z': 89}
 
-ls_dict.append(dict_1)
-ls_dict.append(dict_2)
+ls_dict_test.append(dict_1)
+ls_dict_test.append(dict_2)
 
 # print(ls_dict[1])
 
@@ -37,24 +37,33 @@ data_frame = pd.read_csv(file_name)
 
 ls_dict_changed = []
 
+ 
 def data_procesing ():
 
     test_dict = {}
+    ls_dict = []
     
 
     for index, row in data_frame.iterrows():
-        #print(f"Fila: {index}")
+        # print(f"Fila: {index}")
         #print(f"150: {row['150']}, 4: {row['4']}, versicolor: {row['versicolor']}")
         
-        ls_dict = []
-        test_dict ['150'] = row['150']
-        test_dict ['4'] = row['4']
+        test_dict ['x'] = row['150']
+        test_dict ['y'] = row['4']
         ls_dict.append(test_dict)
 
 
-    return ls_dict
-    
+    return print (ls_dict)
+
 
     
+
+# def dictionary_processed ():
+
+#     data_procesing()
+#     print (ls_dict)
+
+#     # for x in data_procesing.ls_dic
         
 
+# dictionary_processed()
